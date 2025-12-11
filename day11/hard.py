2f6be10a427e5914c3f4e2811da2cbf6760a09d1
nort <vsevolodsevostyanov3@gmail.com>
2025-12-11 11:47:18 +0100

    Day 11 - part 2

--- day11/hard.py
+++ day11/hard.py
@@ -1,3 +1,4 @@
+from functools import cache
 graph_nodes = {}
 paths = []
 with open('input.txt', 'r') as f:
@@ -7,31 +8,28 @@ with open('input.txt', 'r') as f:
         node_paths = labels_paths[1].strip().split(' ')
         graph_nodes[labels_paths[0]] = node_paths
 
-print(graph_nodes)
-
-def look_for_path(node, path_taken):
+@cache
+def look_for_path(node, end_node) -> int:
     if node == 'out':
-        paths.append(path_taken)
-        return
+        if end_node == 'out':
+            return 1
+        return 0
+    if node == end_node:
+        return 1
 
-    path_to_follow = path_taken.copy()
-    path_to_follow.append(node)
+    path_sum = 0
     for path_node in graph_nodes[node]:
-        look_for_path(path_node,path_to_follow)
+        path_sum += look_for_path(path_node, end_node)
 
-look_for_path('svr', [])
+    return path_sum
 
-result = 0
+svr_fft = look_for_path('svr','fft')
+fft_dac = look_for_path('fft','dac')
+dac_out = look_for_path('dac','out')
 
-for path in paths:
-    found_fft = False
-    found_dac = False
-    for node in path:
-        if node == 'fft':
-            found_fft = True
-        if node == 'dac':
-            found_dac = True
-    if found_fft and found_dac:
-        result += 1
+svr_dac = look_for_path('svr','dac')
+dac_fft = look_for_path('dac','fft')
+fft_out = look_for_path('fft','out')
 
+result = svr_fft * fft_dac * dac_out + svr_dac * dac_fft * fft_out
 print(result)
